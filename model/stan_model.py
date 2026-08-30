fb1d8001af2f345e2081a002b3b541be890add18
neystan <2918360904@qq.com>
2026-08-30 23:38:00 +0800

    拼接block

--- model/stan_model.py
+++ model/stan_model.py
@@ -287,4 +287,27 @@ class FeedForward(nn.Module):
     def forward(self, x): 
         return self.dropout(self.down_proj(self.act_fn(self.gate_proj(x)) * self.up_proj(x)))
 
+class StanMindBlock(nn.Module):
+    def __init__(self, layer_id : int, config : StanMindConfig):
+        super().__init__()
+        self.num_attention_heads = config.num_attention_heads
+        self.hidden_size = config.hidden_size
+        self.head_dim = self.hidden_size // self.num_attention_heads
+        self.self_attn = Attention(config)
+
+        self.layer_id = layer_id
+        self.input_layernorm = RMSNorm(config.hidden_size, eps = config.rms_norm_eps)
+        self.post_attention_layernorm = RMSNorm(config.hidden_size, eps = config.rms_norm_eps)
+        self.mlp = FeedForward(config)
+
+    def forward(self, hidden_states, position_embedding, past_key_value = None, use_cache = False, attention_mask = None):
+        residual = hidden_states
+        hidden_states, present_key_value = self.self_attn(
+            self.input_layernorm(hidden_states), position_embedding, past_key_value, use_cache, attention_mask
+        )
+        # 残差处理
+        hidden_states = residual + hidden_states
+        hidden_states = hidden_states + self.mlp(self.post_attention_layernorm(hidden_states))
+        return hidden_states, present_key_value
+
     
